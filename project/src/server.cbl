85a6f71f93099f5c3e3ec9e4a387baecc1295c70
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-06-04 11:44:12 +0100

    slight code changes and neating of code.

--- project/src/server.cbl
+++ project/src/server.cbl
@@ -11,51 +11,52 @@
            FILE SECTION.
            FD F-MESSAGES-FILE.
            01 RC-MESSAGE.
-               05 RC-MESSAGE-ID-NUM PIC 99.
-               05 RC-MESSAGE-TITLE PIC X(60).
-               05 RC-MESSAGE-BODY PIC X(300).
+               05 RC-MSG-ID            PIC 99.
+               05 RC-MSG-TITLE         PIC X(60).
+               05 RC-MSG-BODY          PIC X(300).
+           88 END-OF-MESSAGES-FILE     VALUE HIGH-VALUES.
 
            WORKING-STORAGE SECTION.
-           01 WS-FILE-IS-ENDED PIC 9 VALUE ZERO.
-           01 USER-NAME PIC X(10).
-           01 MENU-CHOICE PIC X.
-           01 MSG-MENU-CHOICE PIC X.
+           01 USER-NAME                PIC X(10).
+           01 MENU-CHOICE              PIC X.
+           01 MSG-MENU-CHOICE          PIC X.
 
            SCREEN SECTION.
            01 LOGIN-SCREEN.
              05 BLANK SCREEN.
-             05 LINE 2 COLUMN 10 VALUE "Makers BBS".
-             05 LINE 4 COLUMN 10 VALUE "What's your name?".
-             05 USER-NAME-FIELD LINE 6 COLUMN 10 PIC X(10)
+             05 LINE 2 COL 10      VALUE "MAKERS BBS".
+             05 LINE 4 COL 10      VALUE "What's your name?".
+             05 USER-NAME-FIELD    LINE 6 COL 10 PIC X(10)
                 USING USER-NAME.
 
            01 MENU-SCREEN
              BACKGROUND-COLOR IS 1.
              05 BLANK SCREEN.
-             05 LINE 2 COLUMN 10 VALUE "Makers BBS".
-             05 LINE 4 COLUMN 10 VALUE "Welcome, ".
-             05 LINE 4 COLUMN 19 PIC X(10) USING USER-NAME.
-             05 LINE 8 COLUMN 10 VALUE "(m) Messages".
-             05 LINE 8 COLUMN 30 VALUE "(l) Logout".
-             05 LINE 8 COLUMN 48 VALUE "(q) Quit".
-             05 LINE 20 COLUMN 10 VALUE "Pick: ".
-             05 MENU-CHOICE-FIELD LINE 20 COLUMN 16 PIC X
-                USING MENU-CHOICE.
+             05 LINE  2 COL  10  VALUE "MAKERS BBS".
+             05 LINE  4 COL  10  VALUE "Welcome, ".
+             05 LINE  4 COL  19  PIC X(10) USING USER-NAME.
+             05 LINE  8 COL  10  VALUE "(m) Messages".
+             05 LINE  8 COL  30  VALUE "(l) Logout".
+             05 LINE  8 COL  48  VALUE "(q) Quit".
+             05 LINE 20 COL  10  VALUE "Pick: ".
+             05 MENU-CHOICE-FIELD  LINE 20 COL 16 PIC X
+                                   USING MENU-CHOICE.
 
            01 MESSAGES-MENU-SCREEN
              BACKGROUND-COLOR IS 1.
              05 BLANK SCREEN.
-             05 LINE 2 COLUMN 10 VALUE "Makers BBS".
-             05 LINE 4 COLUMN 10 VALUE "Here are the last 10 messages:".
-             05 LINE 6 COLUMN 10 PIC X(60) USING RC-MESSAGE-TITLE.
-             05 LINE 26 COLUMN 10 VALUE "( ) Read Message".
-             05 LINE 28 COLUMN 10 VALUE "(w) Write your own message".
-             05 LINE 30 COLUMN 10 VALUE "(n) Next Page".
-             05 LINE 30 COLUMN 30 VALUE "(p) Previous Page".
-             05 LINE 30 COLUMN 50 VALUE "(g) Go back".
-             05 LINE 32 COLUMN 10 VALUE "Pick: ".
-             05 MSG-MENU-CHOICE-FIELD LINE 32 COLUMN 16 PIC X
-                USING MSG-MENU-CHOICE.
+             05 LINE  2 COL 10 VALUE "MAKERS BBS".
+             05 LINE  4 COL 10 VALUE "Here's the last 10 messages:".
+             05 LINE  6 COL 10 USING RC-MSG-TITLE.
+             05 LINE 26 COL 10 VALUE "( ) Read Message by Number".
+             05 LINE 28 COL 10 VALUE "(w) Write A Message".
+             05 LINE 30 COL 10 VALUE "(n) Next Page".
+             05 LINE 30 COL 30 VALUE "(p) Previous Page".
+             05 LINE 30 COL 54 VALUE "(g) Go Back".
+             05 LINE 30 COL 72 VALUE "(q) Quit".
+             05 LINE 32 COL 10 VALUE "Pick: ".
+             05 MSG-MENU-CHOICE-FIELD LINE 32 COL 16 PIC X
+                                      USING MSG-MENU-CHOICE.
 
 
       *    01-VIEW-MESSAGE-SCREEN.
@@ -65,22 +66,19 @@
 
        PROCEDURE DIVISION.
 
-       OPEN INPUT F-MESSAGES-FILE.
-       
        READ-TITLE.
-           PERFORM UNTIL WS-FILE-IS-ENDED = 1
-             READ F-MESSAGES-FILE
-               NOT AT END
-                 DISPLAY
-                   RC-MESSAGE-TITLE
-                 END-DISPLAY
-               AT END  
-                 MOVE 1 TO WS-FILE-IS-ENDED
-             END-READ      
-           END-PERFORM.
-
+           OPEN INPUT F-MESSAGES-FILE
+               READ F-MESSAGES-FILE
+                   AT END SET END-OF-MESSAGES-FILE TO TRUE
+               END-READ
+           PERFORM UNTIL END-OF-MESSAGES-FILE
+               DISPLAY RC-MSG-TITLE
+               READ F-MESSAGES-FILE
+                   AT END SET END-OF-MESSAGES-FILE TO TRUE
+               END-READ
+           END-PERFORM
            CLOSE F-MESSAGES-FILE.
-
+           
        0110-DISPLAY-LOGIN.
            INITIALIZE USER-NAME.
            DISPLAY LOGIN-SCREEN.
@@ -106,6 +104,8 @@
            ACCEPT MSG-MENU-CHOICE-FIELD.
            IF MSG-MENU-CHOICE = "g" THEN
                PERFORM 0120-DISPLAY-MENU
+           ELSE IF MSG-MENU-CHOICE = "q" THEN
+             STOP RUN
            END-IF.
 
       * 0140-VIEW-MESSAGE.
