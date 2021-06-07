3a2bd297ab14d6c7ad90a2fa74d1c15321e2f874
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-06-08 00:07:11 +0100

    Prettified with cool title screen.

--- project/src/server.cbl
+++ project/src/server.cbl
@@ -15,7 +15,7 @@
            FD F-MESSAGES-FILE.
            01 RC-MESSAGE.
                05 RC-MESSAGE-TITLE PIC X(50).
-               05 RC-MESSAGE-BODY PIC X(300).
+               05 RC-MESSAGE-BODY PIC X(50).
 
            WORKING-STORAGE SECTION.
            01 WS-FILE-IS-ENDED PIC 9 VALUE ZERO.
@@ -27,12 +27,12 @@
            01 MSG-MENU-CHOICE2 PIC X.
            01 WRITE-MSG-MENU-CHOICE PIC X.
            01 MSG-TITLE PIC X(50).
-           01 MSG-BODY PIC X(300).
+           01 MSG-BODY PIC X(50).
            01 WS-MESSAGES.
                05 WS-MESSAGE OCCURS 100 TIMES
                ASCENDING KEY IS WS-TITLE
                INDEXED BY MSG-IDX.
-                   10 WS-TITLE PIC X(60).
+                   10 WS-TITLE PIC X(50).
 
            LINKAGE SECTION.
            01 LS-COUNTER UNSIGNED-INT.
@@ -77,7 +77,7 @@
            01 MENU-SCREEN
              BACKGROUND-COLOR IS 1.
              05 BLANK SCREEN.
-             05 LINE  2 COL 10 VALUE "MAKERS BBS" UNDERLINE, BLINK.
+             05 LINE  2 COL 10 VALUE "MAKERS BBS" UNDERLINE.
              05 LINE  4 COL 10 VALUE "Hi, ".
              05 LINE  4 COL 14 PIC X(16) USING USER-NAME.
              05 LINE  6 COL 10 VALUE "Welcome to COBOL The Barbarian's s
@@ -103,60 +103,73 @@
            01 MSG-MENU-SCREEN
              BACKGROUND-COLOR IS 1.
              05 BLANK SCREEN.
-             05 LINE 2 COL 10 VALUE "MAKERS BBS" UNDERLINE, BLINK.
+             05 LINE 2 COL 10 VALUE "MAKERS BBS" UNDERLINE.
              05 LINE 4 COL 10 VALUE "Here are the last 10 messages:".
-             05 LINE 6 COL 10 VALUE "1.  ".
-             05 LINE 6 COL 14 PIC X(50) USING WS-TITLE(OFFSET).
-             05 LINE 7 COL 10 VALUE "2.  ".
-             05 LINE 7 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 1).
-             05 LINE 8 COL 10 VALUE "3.  ".
-             05 LINE 8 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 2).
-             05 LINE 9 COL 10 VALUE "4.  ".
-             05 LINE 9 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 3).
-             05 LINE 10 COL 10 VALUE "5.  ".
-             05 LINE 10 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 4).
-             05 LINE 11 COL 10 VALUE "6.  ".
-             05 LINE 11 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 5).
-             05 LINE 12 COL 10 VALUE "7.  ".
-             05 LINE 12 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 6).
-             05 LINE 13 COL 10 VALUE "8.  ".
-             05 LINE 13 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 7).
-             05 LINE 14 COL 10 VALUE "9.  ".
-             05 LINE 14 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 8).
-             05 LINE 15 COL 10 VALUE "10.  ".
-             05 LINE 15 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 9).
-             05 LINE 20 COL 10 VALUE "( ) Read Message by Number"
+             05 LINE 6 COL 10 VALUE "-----------------------------------
+      -      "---------------------".
+             05 LINE 7 COL 10 VALUE "*********************BULLETIN BOARD
+      -      "*********************" BLINK, HIGHLIGHT, FOREGROUND-COLOR 
+             IS 2.
+             05 LINE 8 COL 10 VALUE "-----------------------------------
+      -      "---------------------".
+             05 LINE  9 COL 10 VALUE "1.  ".
+             05 LINE  9 COL 14 PIC X(50) USING WS-TITLE(OFFSET).
+             05 LINE 10 COL 10 VALUE "2.  ".
+             05 LINE 10 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 1).
+             05 LINE 11 COL 10 VALUE "3.  ".
+             05 LINE 11 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 2).
+             05 LINE 12 COL 10 VALUE "4.  ".
+             05 LINE 12 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 3).
+             05 LINE 13 COL 10 VALUE "5.  ".
+             05 LINE 13 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 4).
+             05 LINE 14 COL 10 VALUE "6.  ".
+             05 LINE 14 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 5).
+             05 LINE 15 COL 10 VALUE "7.  ".
+             05 LINE 15 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 6).
+             05 LINE 16 COL 10 VALUE "8.  ".
+             05 LINE 16 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 7).
+             05 LINE 17 COL 10 VALUE "9.  ".
+             05 LINE 17 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 8).
+             05 LINE 18 COL 10 VALUE "10. ".
+             05 LINE 18 COL 14 PIC X(50) USING WS-TITLE(OFFSET - 9).
+             05 LINE 19 COL 10 VALUE "----------------------------------
+      -      "---------------------".
+             05 LINE 20 COL 10 VALUE "**********************************
+      -      "*********************" FOREGROUND-COLOR IS 2.
+             05 LINE 21 COL 10 VALUE "----------------------------------
+      -      "---------------------".
+             05 LINE 24 COL 10 VALUE "( ) Read Message by Number"
              REVERSE-VIDEO HIGHLIGHT FOREGROUND-COLOR IS 2.  
-             05 LINE 22 COL 10 VALUE "(w) Write your own message"
+             05 LINE 26 COL 10 VALUE "(w) Write your own message"
              REVERSE-VIDEO HIGHLIGHT FOREGROUND-COLOR IS 2.               
-             05 LINE 24 COL 10 VALUE "(n) Next Page"
+             05 LINE 28 COL 10 VALUE "(n) Next Page"
              REVERSE-VIDEO, HIGHLIGHT FOREGROUND-COLOR IS 6.  
-             05 LINE 24 COL 30 VALUE "(p) Previous Page"
+             05 LINE 28 COL 30 VALUE "(p) Previous Page"
              REVERSE-VIDEO, HIGHLIGHT FOREGROUND-COLOR IS 6. 
-             05 LINE 26 COL 10 VALUE "(g)   Go back"
+             05 LINE 30 COL 10 VALUE "(g)   Go back"
              REVERSE-VIDEO, HIGHLIGHT.
-             05 LINE 26 COL 30 VALUE "(q) Quit         "
+             05 LINE 30 COL 30 VALUE "(q) Quit         "
              REVERSE-VIDEO, HIGHLIGHT.
-             05 LINE 28 COL 10 VALUE "Pick: ".
-             05 MSG-MENU-CHOICE-FIELD LINE 34 COL 16 PIC X
+             05 LINE 32 COL 10 VALUE "Pick: ".
+             05 MSG-MENU-CHOICE-FIELD LINE 32 COL 16 PIC X
                 USING MSG-MENU-CHOICE.
 
            01 WRITE-MSG-SCREEN
              BACKGROUND-COLOR IS 1.
              05 BLANK SCREEN.
-             05 LINE 2 COL 10 VALUE "MAKERS BBS" UNDERLINE, BLINK.
-             05 LINE 4 COL 10 VALUE "Title: ".
-             05 MSG-TITLE-FIELD LINE 4 COL 17 PIC X(50)
+             05 LINE 2 COL 10 VALUE "MAKERS BBS" UNDERLINE.
+             05 LINE 6 COL 10 VALUE "Title:   ".
+             05 MSG-TITLE-FIELD LINE 6 COL 17 PIC X(50)
                 USING MSG-TITLE.
-             05 LINE 6 COL 10 VALUE "Body: ".
-             05 MSG-BODY-FIELD LINE 6 COL 16 PIC X(300)
+             05 LINE 8 COL 10 VALUE "Message: ".
+             05 MSG-BODY-FIELD LINE 8 COL 16 PIC X(50)
                 USING MSG-BODY.
-             05 LINE 30 COL 10 VALUE "(g) Go back"
+             05 LINE 14 COL 10 VALUE "(g) Go back"
              REVERSE-VIDEO, HIGHLIGHT.
-             05 LINE 32 COL 10 VALUE "(q)    Quit"
+             05 LINE 16 COL 10 VALUE "(q)    Quit"
              REVERSE-VIDEO, HIGHLIGHT.
-             05 LINE 34 COL 10 VALUE "Pick: ".             
-             05 WRITE-MSG-CHOICE-FIELD LINE 34 COL 16 PIC X
+             05 LINE 18 COL 10 VALUE "Pick: ".             
+             05 WRITE-MSG-CHOICE-FIELD LINE 18 COL 16 PIC X
                 USING WRITE-MSG-MENU-CHOICE.        
                            
        PROCEDURE DIVISION.
